70223fc414e41bfa09d4a2fc6963e0d188df5184
若海 <wang@rehiy.com>
2022-03-15 05:32:27 +0800

    支持更多的状态检测

--- app/app.js
+++ app/app.js
@@ -6,20 +6,20 @@ app.component('app-home', {
             腾讯云技术早报 【${this.getDate()}】
 
             * 科技热点
-            1．{SUBJECT-1}
+            1．{SUBJECT-0}
+            全文链接：{URL-0}
+            2．{SUBJECT-1}
             全文链接：{URL-1}
-            2．{SUBJECT-2}
-            全文链接：{URL-2}
 
             * 腾讯云技术文章
-            1．{SUBJECT-3}
+            1．{SUBJECT-2}
+            全文链接：{URL-2}
+            2．{SUBJECT-3}
             全文链接：{URL-3}
-            2．{SUBJECT-4}
-            全文链接：{URL-4}
         `;
         paper = paper.trim().replace(/\n +/g, '\n');
         return {
-            doing: false,
+            pulling: false,
             ids: ['', ''],
             items: [],
             notice: [],
@@ -29,9 +29,7 @@ app.component('app-home', {
     },
     watch: {
         message(newValue, oldValue) {
-            this.notice = [];
-            this.parserPaper();
-            this.checkLinks();
+            this.pulling || this.pagerParser();
         }
     },
     methods: {
@@ -45,7 +43,8 @@ app.component('app-home', {
             return month + date + '周' + ['日', '一', '二', '三', '四', '五', '六'][day];
         },
         yunPlusArticle() {
-            this.doing = true;
+            this.notice = [];
+            this.pulling = true;
             const api = 'api/article.php?';
             const params = this.ids.map(id => {
                 return 'id[]=' + id;
@@ -53,44 +52,53 @@ app.component('app-home', {
             fetch(api + params.join('&'))
                 .then(response => response.json())
                 .then(data => {
-                    let idx = 3;
-                    let paper = this.paper;
-                    data.forEach(item => {
-                        paper = paper.replace(`{SUBJECT-${idx}}`, item.subject);
-                        paper = paper.replace(`{URL-${idx}}`, item.url);
-                        idx++;
-                    });
-                    this.message = paper.replace(/\{.+\}/g, '');
+                    this.items[0] || (this.items[0] = {});
+                    this.items[1] || (this.items[1] = {});
+                    data[0] && (this.items[2] = data[0]);
+                    data[1] && (this.items[3] = data[1]);
+                    this.pagerRender();
                 })
                 .catch(err => {
                     this.notice.push('文章拉取失败。');
                     console.log('文章拉取失败。', err);
                 })
                 .finally(() => {
-                    this.doing = false;
+                    this.pulling = false;
                 });
         },
         checkLinks() {
+            this.notice = [];
             // 检测重复
             const duplicates = [];
             this.items.forEach(item => {
-                item.NoDuplicate = duplicates.includes(item.link);
-                duplicates.push(item.link);
+                item.NoDuplicate = !duplicates.includes(item.url);
+                duplicates.push(item.url);
             });
             // 检测数量
             if (this.items.length != 4) {
                 this.notice.push('文章数量不正确，或格式错误；请检查换行、空格是否有冗余。');
             }
         },
-        parserPaper() {
-            let item;
-            let message = this.message;
-            const findExp = /\d．(.+)\n全文链接：(http.+\d+)[\s\n]*/;
+        pagerParser() {
             this.items = [];
+            let item, message = this.message;
+            const findExp = /\d．(.+)\n全文链接：(http.+\d+)[\s\n]*/;
             while (item = message.match(findExp)) {
-                this.items.push({ subject: item[1], link: item[2] });
+                this.items.push({ subject: item[1], url: item[2] });
                 message = message.replace(findExp, '');
             }
+            this.checkLinks();
+        },
+        pagerRender() {
+            let paper = this.paper;
+            this.items.forEach((item, idx) => {
+                if (item.subject && item.url) {
+                    paper = paper.replace(`{SUBJECT-${idx}}`, item.subject);
+                    paper = paper.replace(`{URL-${idx}}`, item.url);
+                }
+            });
+            this.message = paper.replace(/\{.+\}/g, '');
+            this.checkLinks();
         }
     },
     template: `
@@ -117,7 +125,7 @@ app.component('app-home', {
                         <div class="d-flex flex-row m-3">
                             <input type="number" class="form-control" placeholder="文章Id 1" v-model="ids[0]" />
                             <input type="number" class="form-control ms-3" placeholder="文章Id 2" v-model="ids[1]" />
-                            <button class="form-control btn btn-secondary ms-3" v-if="doing">Loading</button>
+                            <button class="form-control btn btn-secondary ms-3" v-if="pulling">Pulling</button>
                             <button class="form-control btn btn-primary ms-3" @click="yunPlusArticle()" v-else>确定</button>
                         </div>
                     </div>
@@ -127,12 +135,14 @@ app.component('app-home', {
                     <div class="card mt-3" v-for="item in items">
                         <div class="card-body">
                             <h5 class="card-title">{{item.subject}}</h5>
-                            <a class="card-link" target="_blank" :href="item.link" >{{item.link}}</a>
+                            <a class="card-link" target="_blank" :href="item.url" >{{item.url}}</a>
                         </div>
                         <div class="card-footer text-muted">
-                            <span class="badge bg-success me-3" v-if="!item.StarRequired && !item.NoDuplicate">Success</span>
-                            <span class="badge bg-warning me-3" v-if="item.StarRequired">需关注</span>
-                            <span class="badge bg-danger me-3" v-if="item.NoDuplicate">重复</span>
+                            <span class="badge bg-success me-3" v-if="item.subject && item.url && item.NoDuplicate && !item.StarRequired">PASS</span>
+                            <span class="badge bg-danger me-3" v-if="!item.subject">标题异常</span>
+                            <span class="badge bg-danger me-3" v-if="!item.url">链接异常</span>
+                            <span class="badge bg-warning me-3" v-if="!item.NoDuplicate">重复</span>
+                            <span class="badge bg-primary me-3" v-if="item.StarRequired">需关注</span>
                         </div>
                     </div>
                 </div>
